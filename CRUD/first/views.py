425cdde4f1e17290ef0ba8920373f01d758801b8
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-02-19 12:48:03 +0530

    Add success message display in first_index.html and update movie_dict in views.py

--- CRUD/first/views.py
+++ CRUD/first/views.py
@@ -6,7 +6,8 @@ from django.http import HttpResponse
 movie_dict={
     "title":"Marco",
     "desc":"Adattu is one of the most renowned gold-trading families in Kerala. Unexpectedly, a mysterious incident shakes the Adattu family. George, the head of the family, sets out to uncover the truth and find those responsible. At the same time, his younger brother, Marco, embarks on the same quest but through a different Path. This forms the core of Marco, an intense Violent action-packed drama.",
-    "year":"20 December 2024"
+    "year":"20 December 2024",
+    "success":True,
 }
 def print_welcome(request):
     return render(request,"first_index.html",movie_dict)
